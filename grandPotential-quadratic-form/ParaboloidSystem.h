ec94027b843df5da8106759a1aa4e7a653851662
Xander <atmmensah@gmail.com>
2025-06-06 17:01:46 -0400

    Added BC_AMR.prm file generator
    also added getter for order parameter names.
    reordered scale auto-selection so that energy scale will be calculated even without userInputs

--- grandPotential-quadratic-form/ParaboloidSystem.h
+++ grandPotential-quadratic-form/ParaboloidSystem.h
@@ -212,6 +212,28 @@ public:
   void
   auto_select_scales(const userInputParameters<dim> *userInputs)
   {
+    // If energy_scale is zero, set it to the lowest free energy curvature
+    // TODO: Choose the lowest principal eigenvalue of the curvature matrix instead
+    // ========================================================================
+    if (energy_scale == 0.0)
+      {
+        double minimum_diagonal_curvature = std::numeric_limits<double>::max();
+        for (const auto &phase : phases)
+          {
+            for (unsigned int comp_index = 0; comp_index < num_comps; ++comp_index)
+              {
+                minimum_diagonal_curvature =
+                  std::min(minimum_diagonal_curvature,
+                           phase._A_well(comp_index, comp_index));
+              }
+          }
+        energy_scale = minimum_diagonal_curvature;
+        std::cout << "Setting energy scale to " << energy_scale
+                  << " based on the lowest free energy curvature diagonal.\n";
+      }
+    // ========================================================================
+
+    // If userInputs is null, we cannot auto-select time and length scales
     if (userInputs == nullptr)
       {
         return;
@@ -299,27 +321,6 @@ public:
                       << userInputs->dtValue * reccommended_time_scale / time_scale
                       << " to ensure stability.\n\n";
           }
-      }
-    // ========================================================================
-
-    // If energy_scale is zero, set it to the lowest free energy curvature
-    // TODO: Choose the lowest principal eigenvalue of the curvature matrix instead
-    // ========================================================================
-    if (energy_scale == 0.0)
-      {
-        double minimum_diagonal_curvature = std::numeric_limits<double>::max();
-        for (const auto &phase : phases)
-          {
-            for (unsigned int comp_index = 0; comp_index < num_comps; ++comp_index)
-              {
-                minimum_diagonal_curvature =
-                  std::min(minimum_diagonal_curvature,
-                           phase._A_well(comp_index, comp_index));
-              }
-          }
-        energy_scale = minimum_diagonal_curvature;
-        std::cout << "Setting energy scale to " << energy_scale
-                  << " based on the lowest free energy curvature diagonal.\n";
       }
     // ========================================================================
   }
@@ -449,6 +450,40 @@ public:
   }
 
   /**
+   * @brief Get names of order parameters
+   * @return A vector of order parameter names
+   */
+  std::vector<std::string>
+  get_order_parameter_names() const
+  {
+    std::vector<std::string> op_names;
+    std::map<uint, uint>     phase_counter;
+    for (const auto &phase_index : order_params)
+      {
+        if (phase_counter.find(phase_index) == phase_counter.end())
+          {
+            phase_counter[phase_index] = 0;
+          }
+        std::string phase_name = phase_names[phase_index];
+        std::string var_name =
+          phase_name + "_" + std::to_string(phase_counter[phase_index]);
+        op_names.push_back(var_name);
+        phase_counter[phase_index]++;
+      }
+    return op_names;
+  }
+
+  /**
+   * @brief Get names of components
+   * @return A vector of component names
+   */
+  const std::vector<std::string> &
+  get_component_names() const
+  {
+    return comp_names;
+  }
+
+  /**
    * @brief Declare the fields needed for the PDE in PRISMS-PF
    * @param loader Pointer to the attribute loader (equations.cc)
    */
@@ -469,22 +504,12 @@ public:
     std::cout << "\n"
               << "Order parameter names: ";
     // Get names for order parameter fields
-    std::vector<std::string> op_names;
+    std::vector<std::string> op_names = get_order_parameter_names();
     std::vector<std::string> grad_op_names;
-    std::map<uint, uint>     phase_counter;
-    for (const auto &phase_index : order_params)
+    for (const auto &op_name : op_names)
       {
-        if (phase_counter.find(phase_index) == phase_counter.end())
-          {
-            phase_counter[phase_index] = 0;
-          }
-        std::string phase_name = phase_names[phase_index];
-        std::string var_name =
-          phase_name + "_" + std::to_string(phase_counter[phase_index]);
-        op_names.push_back(var_name);
-        grad_op_names.push_back("grad(" + var_name + ")");
-        phase_counter[phase_index]++;
-        std::cout << var_name << " ";
+        grad_op_names.push_back("grad(" + op_name + ")");
+        std::cout << op_name << " ";
       }
     std::cout << "\n";
 
@@ -536,21 +561,7 @@ public:
         mu_names.push_back("mu_" + comp_name);
       }
     // Get names for order parameter fields
-    std::vector<std::string> op_names;
-    std::map<uint, uint>     phase_counter;
-    for (const auto &phase_index : order_params)
-      {
-        if (phase_counter.find(phase_index) == phase_counter.end())
-          {
-            phase_counter[phase_index] = 0;
-          }
-        std::string phase_name = phase_names[phase_index];
-        std::string var_name =
-          phase_name + "_" + std::to_string(phase_counter[phase_index]);
-        op_names.push_back(var_name);
-        phase_counter[phase_index]++;
-      }
-    std::cout << "\n";
+    std::vector<std::string> op_names = get_order_parameter_names();
 
     // Assign fields
     for (const auto &c_name : c_names)
