65a928c66afb7b44ab0a02fdcdc07346fc09e226
Xander <102053371+fractalsbyx@users.noreply.github.com>
2025-06-27 13:49:44 -0400

    cleanup 2

--- grandPotential-paraboloid/ParaboloidSystem.h
+++ grandPotential-paraboloid/ParaboloidSystem.h
@@ -457,8 +457,14 @@ public:
         loader->set_need_value_nucleation(var_index, true);
         loader->insert_dependencies_value_term_RHS(var_index, mu_names);
         loader->insert_dependencies_value_term_RHS(var_index, grad_mu_names);
+        loader->insert_dependencies_value_term_RHS(var_index, op_names);
+        loader->insert_dependencies_value_term_RHS(var_index, grad_op_names);
+        loader->insert_dependencies_value_term_RHS(var_index, op_detadt_names);
         loader->insert_dependencies_gradient_term_RHS(var_index, mu_names);
         loader->insert_dependencies_gradient_term_RHS(var_index, grad_mu_names);
+        loader->insert_dependencies_gradient_term_RHS(var_index, op_names);
+        loader->insert_dependencies_gradient_term_RHS(var_index, grad_op_names);
+        loader->insert_dependencies_gradient_term_RHS(var_index, op_detadt_names);
         var_index++;
       }
     for (const auto &op_name : op_names)
@@ -467,15 +473,8 @@ public:
         loader->set_variable_type(var_index, SCALAR);
         loader->set_variable_equation_type(var_index, EXPLICIT_TIME_DEPENDENT);
         loader->set_need_value_nucleation(var_index, true);
-        loader->insert_dependencies_value_term_RHS(var_index, mu_names);
-        loader->insert_dependencies_value_term_RHS(var_index, grad_mu_names);
         loader->insert_dependencies_value_term_RHS(var_index, op_names);
-        loader->insert_dependencies_value_term_RHS(var_index, grad_op_names);
         loader->insert_dependencies_value_term_RHS(var_index, op_detadt_names);
-        loader->insert_dependencies_gradient_term_RHS(var_index, mu_names);
-        loader->insert_dependencies_gradient_term_RHS(var_index, grad_mu_names);
-        loader->insert_dependencies_gradient_term_RHS(var_index, op_names);
-        loader->insert_dependencies_gradient_term_RHS(var_index, grad_op_names);
         var_index++;
       }
     for (const auto &op_name : op_names)
@@ -488,7 +487,6 @@ public:
         loader->insert_dependencies_value_term_RHS(var_index, grad_mu_names);
         loader->insert_dependencies_value_term_RHS(var_index, op_names);
         loader->insert_dependencies_value_term_RHS(var_index, grad_op_names);
-        loader->insert_dependencies_value_term_RHS(var_index, op_detadt_names);
         loader->insert_dependencies_gradient_term_RHS(var_index, mu_names);
         loader->insert_dependencies_gradient_term_RHS(var_index, grad_mu_names);
         loader->insert_dependencies_gradient_term_RHS(var_index, op_names);
