757aa3b8e9c208dd0ce4213746a71e6aac8a1e43
Xander <atmmensah@gmail.com>
2025-07-29 18:59:02 -0400

    ic

--- grandPotential-paraboloid/ICs_and_BCs.cc
+++ grandPotential-paraboloid/ICs_and_BCs.cc
@@ -37,12 +37,13 @@ customPDE<dim, degree>::setInitialCondition([[maybe_unused]] const Point<dim>  &
 
   // TODO: Make relevant geometries
   [[maybe_unused]] double circular = interface(0.5 * (r0 * r0 - r2) / r0);
-  [[maybe_unused]] double flat     = interface(0.5 * (r0 * r0 - y * y) / r0);
+  [[maybe_unused]] double flat     = interface(p[1] - r0);
 
   // TODO: Populate eta0 with the initial condition for the order parameters
   std::vector<double> eta0(isoSys.order_params.size(), 0.0);
-  eta0[0] = 1.0 - circular;
-  eta0[1] = circular;
+  eta0[0] = (1.0 - flat) * ((1.0 / 2.0) + 0.1 * dist(rng));
+  eta0[1] = (1.0 - flat) * ((1.0 / 2.0) + 0.1 * dist(rng));
+  eta0[2] = flat;
   // ---------------------------------------------------------------------
   //  < ENTER THE INITIAL CONDITIONS HERE
   // ---------------------------------------------------------------------
